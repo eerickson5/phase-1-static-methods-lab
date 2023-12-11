992aba91918b3bd58af0dc3d12c127b13526f71c
Elise Erickson <elisece717@gmail.com>
2023-12-11 08:43:09 -0500

    satisfes tests for string modifier class

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.slice(0,1).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+
+  static titleize(string){
+    let uncapitalizedStrings = {
+      the: '', 
+      a: '', 
+      an: '', 
+      but: '', 
+      of: '', 
+      and: '', 
+      for: '', 
+      at: '', 
+      by: '',
+      from: ''
+    }
+
+    let array = string.split(" ");
+
+    let newArray = array.map(currString => {
+      if(uncapitalizedStrings[currString] === undefined){
+        return this.capitalize(currString);
+      } else {
+        return currString
+      }
+    })
+
+    newArray[0] = this.capitalize(array[0]);
+
+    return newArray.join(" ")
+  }
 }
